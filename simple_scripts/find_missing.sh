92d9a4e756f5dafd1bf04547bbf140b3f47d8962
Mike Porter <mike@udel.edu>
2023-12-22 12:27:48 -0500

    Simple date change.

--- simple_scripts/find_missing.sh
+++ simple_scripts/find_missing.sh
@@ -16,7 +16,7 @@ sa_r_output=`mktemp sa_r_output_XXXXXX`
 # affected accounts.
 #
 
-DATE_TO_SKIP='Time":"2023-12-'
+DATE_TO_SKIP='Time":"2023-12-1'
 
 cut -d: -f 1 ${GDUSER}.json | sort > ${sa_output}
 fgrep '"ownedByMe":true' ${GDUSER}-r.json | \
